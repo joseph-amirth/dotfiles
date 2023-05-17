b5523fb0ebf8c36c8151af92807f164b05657bdf
joseph-amirth <epicjoe128@gmail.com>
2023-05-17 16:57:26 +0530

    Add lualine to nvim

--- dot_config/nvim/lua/joseph/packer.lua
+++ dot_config/nvim/lua/joseph/packer.lua
@@ -48,4 +48,9 @@ return require('packer').startup(function(use)
 
     use 'simrat39/rust-tools.nvim'
 
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+
 end)
