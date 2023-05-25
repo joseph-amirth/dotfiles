138e4cb88ea860c358dde02febc8baeaa1a0c799
joseph-amirth <epicjoe128@gmail.com>
2023-05-25 09:17:35 +0530

    Clean setup a bit and update setup to install ranger-git from AUR

--- dot_setup/executable_setup.sh
+++ dot_setup/executable_setup.sh
@@ -24,12 +24,12 @@ function wait_for_internet_connection() {
     # Wait for internet connection to be established.
     while ! ping -c1 8.8.8.8 &> /dev/null
     do
-        echo "Ping failed - trying again in 1 second(s)."
-        echo "Check internet connection."
+        errorln "Ping failed - trying again in 1 second(s)."
+        errorln "Check internet connection."
         sleep 1
     done
     
-    echo "Internet connection established."
+    successln "Internet connection established."
 }
 
 function install_prerequisites() {
@@ -61,7 +61,7 @@ function install_de() {
     sudo pacman -S --noconfirm xorg nvidia-open
 
     # Install and enable display manager.
-    sudo pacman -S lightdm lightdm-slick-greeter
+    sudo pacman -S --noconfirm lightdm lightdm-slick-greeter
     sudo systemctl enable lightdm.service
 
     # Install window manager and composite manager.
@@ -76,16 +76,16 @@ function install_utils() {
     # Install and setup backlight management.
     usermod -aG video joseph
     echo "ACTION==\"add\", SUBSYSTEM==\"backlight\", RUN+=\"/bin/chgrp video $sys$devpath/brightness\", RUN+=\"/bin/chmod g+w $sys$devpath/brightness" > /etc/udev/rules.d/backlight.rules
-    sudo pacman -S light
+    sudo pacman -S --noconfirm light
 
     # Install bluetooth protocol stack.
-    sudo pacman -S bluez bluez-utils
+    sudo pacman -S --noconfirm bluez bluez-utils
 
     # Install maim for screenshots.
-    sudo pacman -S maim
+    sudo pacman -S --noconfirm maim
 
     # Install libnotify and notification server.
-    sudo pacman -S libnotify dunst
+    sudo pacman -S --noconfirm libnotify dunst
 }
 
 function setup_terminal() {
@@ -102,7 +102,7 @@ function setup_terminal() {
 
 function install_apps() {
     # Install ranger (file manager).
-    sudo pacman -S --noconfirm ranger
+    paru -S --noconfirm ranger-git
 
     # Install google-chrome.
     paru -S --noconfirm google-chrome
