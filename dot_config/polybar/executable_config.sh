5345ab54868c3ae4e0afce80821dfafa1967699a
joseph-amirth <epicjoe128@gmail.com>
2023-07-22 21:45:00 +0530

    Add support for picking theme

---
+++ dot_config/polybar/executable_config.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export BACKGROUND="#001F1F28"
+export BACKGROUND_ALT="#2A2A37"
+export FOREGROUND="#C8C4AA"
+
+case $1 in
+    space)
+        export PRIMARY="#43C5A5"
+        ;;
+    interstellar)
+        export PRIMARY="#F97583" 
+        ;;
+    tloz)
+        export PRIMARY="#2EA87E"
+        ;;
+    *)
+        export PRIMARY="#7EB3C9"
+esac
+
+export SECONDARY="#8ABEB7"
+
+export ALERT="#A54242"
+export DISABLED="#707880"
+
+# [module/date]
+export DATE_LABEL="%date%"
+export DATE_FORMAT_PREFIX="%{F${FOREGROUND}}  %{F-}"
+export DATE_FORMAT="${DATE_FORMAT_PREFIX}<label>"
