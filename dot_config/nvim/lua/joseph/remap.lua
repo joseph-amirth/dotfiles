f3e4b0db244bfd574a93bac0d8398ed8ea624323
joseph-amirth <epicjoe128@gmail.com>
2023-08-10 23:41:51 +0530

    Add keybindings for nvim-tree commands

--- dot_config/nvim/lua/joseph/remap.lua
+++ dot_config/nvim/lua/joseph/remap.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>pv', vim.cmd.NvimTreeOpen)
+vim.keymap.set('n', '<leader>tt', vim.cmd.NvimTreeToggle)
+vim.keymap.set('n', '<leader>to', vim.cmd.NvimTreeOpen)
+vim.keymap.set('n', '<leader>tc', vim.cmd.NvimTreeClose)
+vim.keymap.set('n', '<leader>tf', vim.cmd.NvimTreeFindFile)
