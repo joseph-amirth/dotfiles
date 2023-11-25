f00e43857fce20bda2eb618b0676e3bce8daf748
joseph-amirth <epicjoe128@gmail.com>
2023-11-25 21:28:53 +0530

    Clean up nvim config

--- dot_config/nvim/after/plugin/format.lua
+++ dot_config/nvim/after/plugin/format.lua
@@ -1,35 +1,13 @@
--- Utilities for creating configurations
+-- TODO: Ensure formatters are installed.
+
 local util = require("formatter.util")
 
--- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
 require("formatter").setup({
-    -- Enable or disable logging
     logging = true,
-    -- Set the log level
     log_level = vim.log.levels.WARN,
-    -- All formatter configurations are opt-in
     filetype = {
         lua = {
-            -- "formatter.filetypes.lua" defines default configurations for the
-            -- "lua" filetype
             require("formatter.filetypes.lua").stylua,
-
-            -- You can also define your own configuration
-            function()
-                -- Full specification of configurations is down below and in Vim help
-                -- files
-                return {
-                    exe = "stylua",
-                    args = {
-                        "--search-parent-directories",
-                        "--stdin-filepath",
-                        util.escape_path(util.get_current_buffer_file_path()),
-                        "--",
-                        "-",
-                    },
-                    stdin = true,
-                }
-            end,
         },
         javascript = {
             require("formatter.filetypes.javascriptreact").prettier,
