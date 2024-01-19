87d297333af075d2a7dde6fd0450e1e87f7e7271
joseph-amirth <epicjoe128@gmail.com>
2024-01-19 22:58:09 +0530

    Add comment to ensure_installed in treesitter config

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -2,18 +2,19 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+        opts = {
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "comment" },
 
-                sync_install = false,
+            sync_install = false,
 
-                auto_install = true,
+            auto_install = true,
 
-                highlight = {
-                    enable = true,
-                },
-            })
+            highlight = {
+                enable = true,
+            },
+        },
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
         end,
     },
 }
