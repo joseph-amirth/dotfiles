936b2cff8e3f8c6d9385a7938349cb38710de08b
joseph-amirth <epicjoe128@gmail.com>
2024-10-31 12:47:45 +0530

    Update hyprpaper config

--- dot_config/hypr/scripts/executable_set-theme.sh
+++ dot_config/hypr/scripts/executable_set-theme.sh
@@ -16,44 +16,54 @@ else
     fatalln "Invalid theme."
 fi
 
-inject_color() {
-    key=$1
-    file=$2
-    value=$(cat ~/.config/hypr/themes/$THEME/color.json | jq -r .$1)
-    exp="s/\{\{$key\}\}/$value/"
-    sed -i -r "$exp" "$file"
-}
+COLORS_JSON=$(cat ~/.config/hypr/themes/$THEME/color.json)
+BG=$(echo $COLORS_JSON | jq -r .bg)
+BG_ALT=$(echo $COLORS_JSON | jq -r .bg_alt)
+FG=$(echo $COLORS_JSON | jq -r .fg)
+PRIMARY=$(echo $COLORS_JSON | jq -r .primary)
+SECONDARY=$(echo $COLORS_JSON | jq -r .secondary)
+ACTIVE=$(echo $COLORS_JSON | jq -r .active)
+DISABLED=$(echo $COLORS_JSON | jq -r .disabled)
 
 set_waybar_theme() {
-    # Generate style.css from style.css.tmpl.
-    template=~/.config/waybar/style.css.tmpl
-    file=~/.config/waybar/style.css
-    cp -f "$template" "$file"
-    inject_color bg "$file"
-    inject_color bg_alt "$file"
-    inject_color primary "$file"
+    sed -i \
+        -e "s/@define-color bg .*/@define-color bg $BG;/" \
+        -e "s/@define-color bg-alt .*/@define-color bg-alt $BG_ALT;/" \
+        -e "s/@define-color fg .*/@define-color fg $FG;/" \
+        -e "s/@define-color primary .*/@define-color primary $PRIMARY;/" \
+        -e "s/@define-color secondary .*/@define-color secondary $SECONDARY;/" \
+        -e "s/@define-color active .*/@define-color active $ACTIVE;/" \
+        -e "s/@define-color disabled .*/@define-color disabled $DISABLED;/" \
+        ~/.config/waybar/style.css
 
-    # Restart waybar.
     killall waybar
     waybar &!
 }
 
-set_wallpaper() {
-    # Generate hyprpaper.conf from hyprpaper.conf.tmpl.
-    template=~/.config/hypr/hyprpaper.conf.tmpl
-    file=~/.config/hypr/hyprpaper.conf
-    cp -f "$template" "$file"
-    exp="s/\{\{theme\}\}/$THEME/"
-    sed -i -r "$exp" "$file"
-
-    # Set wallpaper.
-    hyprctl hyprpaper preload "~/.config/hypr/themes/$THEME/wallpaper.png"
-    hyprctl hyprpaper wallpaper "HDMI-A-2,~/.config/hypr/themes/$THEME/wallpaper.png"
-    ln -sf ~/.config/hypr/themes/$THEME/wallpaper.png ~/.theme/wallpaper
+set_rofi_theme() {
+    sed -i \
+        -e "s/bg: .*;/bg: $BG;/" \
+        -e "s/bg-alt: .*;/bg-alt: $BG_ALT;/" \
+        -e "s/fg: .*;/fg: $FG;/" \
+        -e "s/primary: .*;/primary: $PRIMARY;/" \
+        -e "s/secondary: .*;/secondary: $SECONDARY;/" \
+        -e "s/active: .*;/active: $ACTIVE;/" \
+        -e "s/disabled: .*;/disabled: $DISABLED;/" \
+        ~/.config/rofi/config.rasi
 }
 
-[[ ! -d ~/.theme ]] && mkdir ~/.theme
+set_wallpaper() {
+    IMG=~/.config/hypr/themes/$THEME/wallpaper.png
+    hyprctl hyprpaper preload $IMG
+    hyprctl hyprpaper wallpaper "HDMI-A-2,$IMG"
+
+    sed -i \
+        -e "s/themes\/.*\//themes\/$THEME\//" \
+        ~/.config/hypr/hyprpaper.conf
+}
+
+echo -n $THEME > ~/.theme
 
 set_waybar_theme >/dev/null 2>&1
+set_rofi_theme >/dev/null 2>&1
 set_wallpaper >/dev/null 2>&1
-echo -n $THEME > ~/.theme/name
