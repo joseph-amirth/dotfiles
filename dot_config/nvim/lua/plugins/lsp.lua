ee24f286cf7c8edf44c0565ccfb64994e280add5
Joseph Amirtharaj <epicjoe128@gmail.com>
2024-12-31 11:06:18 +0000

    Update neovim config.
    * Simplify directory structure by reducing nesting.
    * Update mappings for completion config.
    * Lazy-load nvim-tree.
    * Remove neodev and dashboard plugins.

---
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,71 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "mason",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local mason_lspconfig = require "mason-lspconfig"
+      mason_lspconfig.setup {
+        ensure_installed = { "lua_ls", "bashls" },
+      }
+      mason_lspconfig.setup_handlers {
+        function(server_name)
+          if server_name == "tsserver" then
+            server_name = "ts_ls"
+          end
+          require("lspconfig")[server_name].setup { capabilities = capabilities }
+        end,
+      }
+
+      require("lspconfig").lua_ls.setup {
+        settings = {
+          Lua = {
+            completion = {
+              callSnippet = "Replace",
+            },
+          },
+        },
+      }
+
+      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+      vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(event)
+          local function map(keys, func, desc)
+            vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+          end
+
+          map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+          map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+          map("gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementations")
+          map("gD", vim.lsp.buf.definition, "[G]oto [D]eclaration")
+          map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+          map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+          map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+          map("K", vim.lsp.buf.hover, "Hover documentation")
+
+          map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+          map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+        end,
+      })
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    name = "mason",
+    opts = {},
+    config = function(_, opts)
+      require("mason").setup(opts)
+    end,
+  },
+}
---
+++ dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  name = "nvim-tree",
+  opts = {
+    actions = {
+      open_file = {
+        quit_on_open = true,
+      },
+    },
+    view = {
+      relativenumber = true,
+      width = 35,
+    },
+  },
+  keys = {
+    {
+      mode = "n",
+      "<leader>tt",
+      desc = "nvim-tree: Toggle find file",
+      function()
+        require("nvim-tree.api").tree.toggle { find_file = true, open = true, focus = true }
+      end,
+    },
+  },
+}
