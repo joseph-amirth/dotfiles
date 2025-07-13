61edb82f8d9a4c9746dd6a60f737834fbf305423
joseph-amirth <epicjoe128@gmail.com>
2025-07-13 16:28:48 +0530

    Add quit hyprland option and make lock default in power menu

--- dot_config/hypr/scripts/executable_power-menu.sh
+++ dot_config/hypr/scripts/executable_power-menu.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-SHUTDOWN="  Shutdown"
 LOCK="  Lock"
+SHUTDOWN="  Shutdown"
 RESTART="  Restart"
 LOGOUT="󰗼  Log out"
+QUIT="  Quit Hyprland"
 
-ACTIONS="$SHUTDOWN\n$LOCK\n$RESTART\n$LOGOUT"
+ACTIONS="$LOCK\n$SHUTDOWN\n$RESTART\n$LOGOUT\n$QUIT"
 
 ACTION=$(echo -ne "$ACTIONS" | rofi -config power-menu.rasi -dmenu -i)
 
 case $ACTION in
-    $SHUTDOWN)
-        shutdown -h now
-        ;;
     $LOCK)
         loginctl lock-session
+        ;;
+    $SHUTDOWN)
+        shutdown -h now
         ;;
     $RESTART)
         reboot
         ;;
     $LOGOUT)
         loginctl kill-user $(whoami)
+        ;;
+    $QUIT)
+        hyprctl dispatch exit
         ;;
     "")
         ;;
